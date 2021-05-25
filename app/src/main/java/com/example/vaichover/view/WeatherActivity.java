9bbbe04eb1227bd32313d0e9353d8e9b7dc83aa4
unknown <alexandre.alves@ucsal.edu.br>
2021-05-25 19:33:34 -0300

    First commit

---
+++ app/src/main/java/com/example/vaichover/view/WeatherActivity.java
@@ -0,0 +1,16 @@
+package com.example.vaichover.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.vaichover.R;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_weather);
+    }
+}
